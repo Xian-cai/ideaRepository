d0d6874afffaac178e75f3dae253c05dc616fe12
Xian-cai <1786134609@qq.com>
2021-01-29 21:06:16 +0800

    test

---
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+@RequestMapping("/hello")
+public class HelloController {
+
+    @RequestMapping("/hl")
+    @ResponseBody
+    public String hello(){
+        return "hello,world";
+    }
+
+
+}
