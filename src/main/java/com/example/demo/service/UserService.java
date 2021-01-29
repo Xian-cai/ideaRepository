7ffac731b6ebeebc00b196da0713abe62b604b36
Xian-cai <1786134609@qq.com>
2021-01-29 21:35:41 +0800

    新增加了一个UserService

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -1,10 +1,22 @@
 package com.example.demo.service;
 
+import com.example.demo.pojo.User;
+
+import java.util.List;
+
 public interface UserService {
 
-    //查找所有用户的方法
-    void findAllStudent();
+    //查找所有用户的方法,返回一个用户集合
+    List<User>  findAllStudent();
 
     //添加方法
-    void add();
+   int addUser(User user);
+
+    //删除方法
+    int deleteUser(Integer id);
+
+    //修改方法
+
+    int updateUser(User user);
+
 }
